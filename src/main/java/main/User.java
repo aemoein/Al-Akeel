3cefe1e84483bfb19c0dbc6b93028e23f046ca83
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-05-20 17:31:26 +0300

    User Class completed
    The User Entity class is fully completed with all the variables, the setters and getters

--- src/main/java/main/User.java
+++ src/main/java/main/User.java
@@ -1,5 +1,60 @@
 package main;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String name;
+    private String password;
+    private String role;
+
+    // Default constructor (required by Java Persistence API)
+    public User() {
+    }
+
+    // Constructor
+    public User(String name, String password, String role) {
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
