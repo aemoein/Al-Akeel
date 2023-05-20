9b048e0d765ba1e5491332bf0828c578b9cae0ad
AhmedSami36 <119262462+AhmedSami36@users.noreply.github.com>
2023-05-20 18:09:26 +0300

    update class restaurant
    add implementation for restaurant attributes and adding its setters and getters functions

--- src/main/java/main/Restaurant.java
+++ src/main/java/main/Restaurant.java
@@ -1,5 +1,63 @@
 package main;
 
-public class Restaurant {
+import java.sql.RowId;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Restaurant {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+	@Column(name = "ownerId")
+    private Long ownerId;
+	@Column(name = "name")
+	private String Name;
+	
+	private Meal meal = new Meal();
+	
+	// default constructor 
+	public Restaurant() {}
+	
+	public Restaurant(Long Id , Long OwnerId, String resName)
+	{
+		this.Id = Id;
+		this.ownerId = OwnerId;
+		this.Name = resName;
+	}
+	
+	public void setName(String name)
+	{
+		this.Name = name;
+	}
+	public String getNmString()
+	{
+		return this.Name;
+	}
+	
+	
+	public void setId(long id)
+	{
+		this.Id=id;
+	}
+	public long getId()
+	{
+		return this.Id;
+	}
+	
+	
+	public void setOwnerId(long ownerId)
+	{
+		this.ownerId = ownerId;
+	}
+	public long getownerId()
+	{
+		return this.ownerId;
+	}
+	
 }
