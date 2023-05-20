1e45ae8324bd5f58ce205c64b7d86d061ae69aa9
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-05-20 18:17:41 +0300

    class Meal
    The meal class completed and mapped the variables to the database.
    
    All setters and getters are completed.

--- src/main/java/main/Meal.java
+++ src/main/java/main/Meal.java
@@ -1,5 +1,69 @@
 package main;
 
-public class Meal {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Meal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private float price;
+
+    @Column(name = "fk_restaurantId")
+    private Long restaurantId;
+
+    public Meal() 
+    {
+    	
+    }
+
+    // Constructor
+    public Meal(String name, float price, Long restaurantId) {
+        this.name = name;
+        this.price = price;
+        this.restaurantId = restaurantId;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Long getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(Long restaurantId) {
+        this.restaurantId = restaurantId;
+    }
 }
