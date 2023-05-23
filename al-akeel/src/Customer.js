3c042c3575bc5c45fee8413046d1e77526bef755
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-05-23 17:32:48 +0300

    React Frontend
    react interface for the customer and the runner

--- al-akeel/src/Customer.js
+++ al-akeel/src/Customer.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const Customer = () => {
+  const [orderId, setOrderId] = useState('');
+  const [orderStatus, setOrderStatus] = useState('');
+  const history = useHistory();
+
+  const handleOrderIdChange = (e) => {
+    setOrderId(e.target.value);
+  };
+
+  const handleCreateOrder = () => {
+    // Handle create order logic
+  };
+
+  const handleEditOrder = () => {
+    // Handle edit order logic
+  };
+
+  const handleCancelOrder = () => {
+    // Handle cancel order logic
+  };
+
+  const handleListRestaurants = () => {
+    // Handle list restaurants logic
+  };
+
+  const handleBackButtonClick = () => {
+    history.push('/'); // Navigate back to home page
+  };
+
   return (
     <div>
       <h2>Welcome, Customer!</h2>
-      {/* Add customer-specific content */}
+      <h3>Choose what you would like to do:</h3>
+      <div>
+        <label htmlFor="orderId">Order ID:</label>
+        <input type="text" id="orderId" value={orderId} onChange={handleOrderIdChange} />
+      </div>
+      <div>
+        <button onClick={handleCreateOrder}>Create Order</button>
+        <button onClick={handleEditOrder}>Edit Order</button>
+        <button onClick={handleCancelOrder}>Cancel Order</button>
+        <button onClick={handleListRestaurants}>List Restaurants</button>
+      </div>
+      {orderStatus && <p>Order Status: {orderStatus}</p>}
+      <button onClick={handleBackButtonClick}>Back</button>
     </div>
   );
 };
--- al-akeel/src/Runner.js
+++ al-akeel/src/Runner.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const Runner = () => {
+  const [orderId, setOrderId] = useState('');
+  const [orderStatus, setOrderStatus] = useState('');
+  const [completedTrips, setCompletedTrips] = useState(0);
+  const history = useHistory();
+
+  const handleOrderIdChange = (e) => {
+    setOrderId(e.target.value);
+  };
+
+  const handleAcceptOrder = () => {
+    // Handle accept order logic
+  };
+
+  const handleCancelOrder = () => {
+    // Handle cancel order logic
+  };
+
+  const handleCompleteOrder = () => {
+    // Handle complete order logic
+  };
+
+  const handleGetCompletedTrips = () => {
+    // Handle get completed trips logic
+  };
+
+  const handleGetTripHistory = () => {
+    // Handle get trip history logic
+  };
+
+  const handleBackButtonClick = () => {
+    history.push('/'); // Navigate back to home page
+  };
+
   return (
     <div>
       <h2>Welcome, Runner!</h2>
-      {/* Add runner-specific content */}
+      <h3>Choose what you would like to do:</h3>
+      <div>
+        <label htmlFor="orderId">Order ID:</label>
+        <input type="text" id="orderId" value={orderId} onChange={handleOrderIdChange} />
+      </div>
+      <div>
+        <button onClick={handleAcceptOrder}>Accept Order</button>
+        <button onClick={handleCancelOrder}>Cancel Order</button>
+        <button onClick={handleCompleteOrder}>Complete Order</button>
+        <button onClick={handleGetCompletedTrips}>Get Completed Trips</button>
+        <button onClick={handleGetTripHistory}>Get Trip History</button>
+      </div>
+      {orderStatus && <p>Order Status: {orderStatus}</p>}
+      <p>Completed Trips: {completedTrips}</p>
+      <button onClick={handleBackButtonClick}>Back</button>
     </div>
   );
 };
